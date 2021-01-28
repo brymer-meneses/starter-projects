5dd87425eba23ac29bf47183aedefac8bf4f314c
Brymer-Meneses <meneses.brymer@gmail.com>
2021-01-28 22:44:45 +0800

    initial commit

--- rust/guessing_game/target/debug/deps/guessing_game.d
+++ rust/guessing_game/target/debug/deps/guessing_game.d
@@ -1,5 +1,5 @@
-C:\Programming_Files\Rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+C:\Programming_Files\Github\Repositories\starter-projects\rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
 
-C:\Programming_Files\Rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+C:\Programming_Files\Github\Repositories\starter-projects\rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
 
 src\main.rs:
--- rust/guessing_game/target/debug/guessing_game.d
+++ rust/guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-C:\Programming_Files\Rust\guessing_game\target\debug\guessing_game.exe: C:\Programming_Files\Rust\guessing_game\src\main.rs
+C:\Programming_Files\Github\Repositories\starter-projects\rust\guessing_game\target\debug\guessing_game.exe: C:\Programming_Files\Github\Repositories\starter-projects\rust\guessing_game\src\main.rs build.rs
