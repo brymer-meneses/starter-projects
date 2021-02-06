b1bba4e1d4827e73210d30ab55b8256dbb40049d
Brymer-Meneses <meneses.brymer@gmail.com>
2021-02-06 18:28:51 +0800

    add changes

--- rust/game-of-life/src/main.rs
+++ rust/game-of-life/src/main.rs
@@ -25,10 +25,75 @@ fn initialize_grid(grid: &mut Vec<Vec<usize>>, size_x: &usize, size_y: &usize) -
 
 }
 
-fn main() {
+fn count_neighbors(grid: &Vec<Vec<usize>>, index_x: i16, index_y: i16) -> i16 {
+    
+    // Counts the number of alive neighbors of a cell
+    // x x o 
+    // o c x 
+    // o o x
+    let mut alive_neightbors = 0;
+
+    for i in -1..1 {
+        for j in -1..1 {
+            
+            if grid[(index_y + i) as usize][(index_x + j) as usize] == 1 {
+                alive_neightbors += 1;
+            }
+        }
+    };
+
+
+    alive_neightbors - 1
+
+
+}
+
+fn next_iter(grid: &mut Vec<Vec<usize>>, size_x: usize, size_y: usize) {
+
+    for i in 0..size_y {
+        for j in 0..size_x {
+            let total_neighbors = count_neighbors(&grid, j as i16, i as i16);
+            
+            if grid[i][j] == 1 {
+                
+            }
+        }
+    }
+}
+
+fn display_grid(grid: &Vec<Vec<usize>>, size_x: usize, size_y: usize) {
 
     let dead_cell = White.on(White).paint(".");
     let alive_cell = Black.on(Black).paint(".");
+
+
+    for i in 0..size_y {
+        for j in 0..size_x {
+            
+            if grid[i][j] == 0 {
+
+                if j % size_x == 0 {
+                    print!("\n{}", dead_cell);
+                } else {
+                    print!("{}", dead_cell); 
+                }
+            }  else if grid[i][j] == 1 { 
+                if j % size_x == 0 {
+                    print!("\n{}", alive_cell);
+                } else {
+                    print!("{}", alive_cell); 
+                }
+            } 
+      }
+    } 
+
+}
+
+fn main() {
+
+    // clear terminal
+    print!("\x1B[2J\x1B[1;1H");
+
     
     println!("Game of Life");
     // Get input from user 
@@ -61,27 +126,7 @@ fn main() {
     let mut grid = vec![vec![0; size_x]; size_y];  
     
     // Random Initialization
+    grid = initialize_grid(&mut grid, &size_x, &size_y);
 
-    let grid = initialize_grid(&mut grid, &size_x, &size_y);
-
-    for i in 0..size_y {
-        for j in 0..size_x {
-            
-            if grid[i][j] == 0 {
-
-                if j % size_x == 0 {
-                    print!("\n{}", dead_cell);
-                } else {
-                    print!("{}", dead_cell); 
-                }
-            }  else if grid[i][j] == 1 { 
-                if j % size_x == 0 {
-                    print!("\n{}", alive_cell);
-                } else {
-                    print!("{}", alive_cell); 
-                }
-            } 
-      }
-    } 
-
+    display_grid(&grid, size_x, size_y);
 }
