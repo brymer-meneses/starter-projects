33cbc9b9014d6db3bcd64036127ffea4bb87545b
Brymer Meneses <meneses.brymer@gmail.com>
2021-06-26 01:40:27 +0800

    autograd.c now works!

--- C/autograd.c
+++ C/autograd.c
@@ -2,115 +2,148 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
+typedef struct node {
+    struct variable *var;
+    double grad;
+} node;
 
-
-typedef struct Variable {
+typedef struct variable {
     double value;
     double grad;
     bool requires_grad;
+    node *parent1;
+    node *parent2;
+} variable;
 
-    struct Variable *parent1;
-    double v1_grad;
-    struct Variable *parent2;
-    double v2_grad;
-} Variable;
+variable *_init_variable(double value, bool requires_grad, node *parent1, node *parent2) {
+    variable *new_variable = malloc(sizeof *new_variable);
 
+    if (new_variable) {
+        new_variable->value = value;
+        new_variable->requires_grad = requires_grad;
+        new_variable->parent1 = parent1;
+        new_variable->parent2 = parent2;
 
-Variable *create_variable(double value, bool requires_grad) {
-    Variable* result = malloc(sizeof(Variable));
-    if (result != NULL) {
-        result->requires_grad = requires_grad;
-        result->value = value;
-        result->parent1 = NULL;
-        result->parent2 = NULL;
-    }
-    return result;
-}
-
-
-Variable *mul(Variable v1, Variable v2) {
-    double result = v1.value * v2.value;
-    Variable* new_variable = malloc(sizeof(Variable));
-
-    if (v1.requires_grad || v2.requires_grad) {
-        new_variable->requires_grad = true;
-    } else {
-        new_variable->requires_grad = false;
-    }
-
-    if (v1.requires_grad) {
-        new_variable->parent1 = &v1;
-        new_variable->v1_grad = v2.value;
-    }
-
-    if (v2.requires_grad) {
-        new_variable->parent2 = &v2;
-        new_variable->v2_grad = v1.value;
-    }
-
-    return new_variable;
-}
-
-Variable *add(Variable v1, Variable v2) {
-    double result = v1.value + v2.value;
-    Variable* new_variable = malloc(sizeof(Variable));
-
-    if (v1.requires_grad || v2.requires_grad) {
-        new_variable->requires_grad = true;
-    } else {
-        new_variable->requires_grad = false;
-    }
-
-    if (v1.requires_grad) {
-        new_variable->parent1 = &v1;
-        new_variable->v1_grad = (double)1.0;
-    }
-
-    if (v2.requires_grad) {
-        new_variable->parent2 = &v2;
-        new_variable->v2_grad = (double)1.0;
-    }
-
-    new_variable->value = result;
-    return new_variable;
-}
-
-void compute_grads(Variable v, double prev_grad) {
-    Variable *dependents[2] = {v.parent1, v.parent2};
-    double grads[2] = {v.v1_grad, v.v2_grad};
-
-    for (int i=0; i<2; i++) {
-        Variable *next_variable = dependents[i];
-        double grad = grads[i];
-
-        if (next_variable->requires_grad) {
-            double this_variable_grads = prev_grad * grad;
-            next_variable->grad = this_variable_grads + next_variable->grad;
-            compute_grads(*next_variable, this_variable_grads);
+        if (requires_grad) {
+            new_variable->grad = 0.0;
         } else {
-            continue;
+            new_variable->grad = NAN;
         }
-
-    };
-
+    }
+    return new_variable;
 }
 
-void backprop(Variable v) {
-    if (v.requires_grad == false) {
-        printf("Called backprop on a variable that does not requires_grad");
-        exit(0);
-    };
-    compute_grads(v, (double)1);
+variable *mul(variable *v1, variable *v2) {
+    double result = v1->value * v2->value;
+    bool requires_grad = v1->requires_grad || v2->requires_grad;
+    node *p1 = NULL;
+    node *p2 = NULL;
+
+    if (v1->requires_grad) {
+        p1 = malloc(sizeof *p1);
+        if (p1) {
+            p1->var = v1;
+            p1->grad = v2->value;
+        } else{
+            printf("Mem Alloc Failure");
+            exit(1);
+        }
+    } 
+    if (v2->requires_grad) {
+        p2 = malloc(sizeof *p2);
+        if (p2) {
+            p2->var = v2;
+            p2->grad = v1->value;
+        } else{
+            printf("Mem Alloc Failure");
+            exit(1);
+        }
+    }
+
+    return _init_variable(result, requires_grad, p1, p2);
+}
+
+variable *add(variable *v1, variable *v2) {
+    double result = v1->value + v2->value;
+    bool requires_grad = v1->requires_grad || v2->requires_grad;
+    node *p1 = NULL;
+    node *p2 = NULL;
+
+    if (v1->requires_grad) {
+        p1 = malloc(sizeof *p1);
+        if (p1) {
+            p1->var = v1;
+            p1->grad = 1.0;
+        } else{
+            printf("Mem Alloc Failure");
+            exit(1);
+        }
+    } 
+    if (v2->requires_grad) {
+        p2 = malloc(sizeof *p2);
+        if (p2) {
+            p2->var = v2;
+            p2->grad = 1.0;
+        } else{
+            printf("Mem Alloc Failure");
+            exit(1);
+        }
+    }
+
+    return _init_variable(result, requires_grad, p1, p2);
+}
+
+void _calculate_grad(variable *v, double grad) {
+    if (v->parent1 != NULL) {
+        variable *next_variable = v->parent1->var;
+        if (next_variable->requires_grad) {
+            node *p1 = v->parent1;
+            double this_variable_grads = grad * p1->grad;
+            next_variable->grad += this_variable_grads;
+            _calculate_grad(next_variable, this_variable_grads);
+        }
+    }
+    
+    if (v->parent2 != NULL) {
+        variable *next_variable = v->parent2->var;
+        if (next_variable->requires_grad) {
+            node *p2 = v->parent2;
+            double this_variable_grads = grad * p2->grad ;
+            next_variable->grad += this_variable_grads;
+            _calculate_grad(next_variable, this_variable_grads);
+        }
+    }
+}
+
+variable *var(double value, bool requires_grad) {
+    return _init_variable(value, requires_grad, NULL, NULL);
+}
+
+void backprop(variable *v1) {
+    _calculate_grad(v1, 1.0);
 }
 
 int main() {
-    Variable *v2 = create_variable(1.0, true);
-    Variable *v1 = create_variable(2.0, true);
+    variable *x = var(5.0, true);
+    variable *y = var(4.0, true);
+    variable *z = var(10.0, true);
 
-    Variable *result = add(*v1, *v2);
+    variable *a = mul(x, y);
+    variable *b = mul(a, x);
 
-    printf("%f",result->value);
-    backprop(*result);
-    return 0;
+    backprop(b);
+
+    // a = xy
+    // b = xa
+    //   = x(xy)
+    //   == x^2 y
+    // dB/dx = 2xy
+    //       = 2(5)(4)
+    //       = 40
+
+    printf("%f\n", x->value);
+    printf("%f\n", x->grad); // 40
 }
+
