0fbe00f72037410c20c59575a29611832560c79c
Brymer <meneses.brymer@gmail.com>
2022-10-12 14:21:49 +0800

    `printer.asm` finally working

--- Assembly/printer.asm
+++ Assembly/printer.asm
@@ -1,103 +1,112 @@
-; int num = 125
-; int divisor = 1
-;
-; while (divisor < num) {
-;   divisor *= 10;
-; }
-;
-; divisor /= 10;
-;
-; while (divisor != 0) {
-;   print ( num/divisor );
-;   divisor /= 10;
-; }
-;
 
-section .bss
-  current_digit resd 1
-  current_divisor resd 1
+; PSEUDOCODE
+; num = 12345
+; temp = num
+; divisor = 1
+; len = 1
+;
+; while temp > 10:
+;     divisor *= 10
+;     temp /= 10
+;     len += 1
+;
+; for (int i=len; i != 0; i--):
+;     digit = (num // divisor) % 10
+;     print(digit)
+;     divisor //= 10
+
 
 section .data
+  num     dd 1234591003
   divisor dd 1
-  num dd 123
-
+  len     dd 1
   newline db 10
+
+section .bss
+  temp    resd 1
+  digit   resd 1
 
 section .text
   global _start
 
 _start:
-  mov ecx, 0
-  mov edx, 0
 
+  ; temp = num
   mov eax, [num]
-  mov ecx, [divisor]
+  mov [temp], eax
 
+  ; get highest multiple of 10 less than num
+  ; while temp > 10 do this
   while1:
-    cmp ecx, eax
-    jge end_while1
+    mov eax, [temp]
+    cmp eax, 10
+    jle end_while1
 
-    ; https://www.youtube.com/watch?v=4OP8p0owPUQ
-    mov edx, 0
-    mov eax, 10
-    mov ecx, [divisor]
-    mul ecx
-
+    ; divisor *= 10
+    mov eax, [divisor]
+    mov ecx, 10
+    mul ecx,
     mov [divisor], eax
+
+    ; temp /= 10
+    mov edx, 0
+    mov eax, [temp]
+    mov ecx, 10
+    div ecx
+    mov [temp], eax
+
+    ; len += 1
+    mov eax, [len]
+    inc eax
+    mov [len], eax
 
     jmp while1
 
   end_while1:
-  
-  mov edx, 0
-  mov eax, [divisor]     ; dividend
-  mov ecx, 10 ; divisor
-  div ecx
-
-  mov [divisor], eax
-
 
   while2:
-    cmp ecx, 0
-    je end_while2
-
-    ; check if current_divisor is 0
+    mov eax, [len]
     cmp eax, 0
-    je end_while2
+    jle end_while2
 
+    ; 1. compute (num/divisor) % 10
 
-    mov eax, 0
-    mov ebx, 0
-    mov ecx, 0
+    ; num/divisor
     mov edx, 0
-
-    ; need to zero edx register before performing the division
-    ; https://stackoverflow.com/questions/45506439/division-of-two-numbers-in-nasm
-    mov edx, 0 
-    mov eax, [num]     ; dividend
-    mov ecx, [divisor] ; divisor
+    mov eax, [num]
+    mov ecx, [divisor]
     div ecx
 
-    mov [num], edx
-    add eax, 48 ; quotine is stored in eax, add 48 to display numbers
-    mov [current_digit], eax
+    ; (num/divisor) is stored in eax
+    mov edx, 0
+    mov ecx, 10
+    div ecx
 
+    ; add and get the remainder which is equivalent to mod10
+    add edx, 48
+    mov [digit], edx
+
+    ; 2. Print value from step 1
     mov eax, 4
     mov ebx, 1
-    mov ecx, current_digit
+    mov ecx, digit
     mov edx, 1
     int 0x80
 
-    ; divide the divisor by 10
-    mov edx, 0 
-    mov eax, [divisor] ; dividend
-    mov ecx, 10                ; divisor
+    ; 3. Continuously divide divisor by 10
+    mov edx, 0
+    mov eax, [divisor]
+    mov ecx, 10
     div ecx
+    mov [divisor], eax
 
-    mov [divisor], eax ; update the value of current_divisor
+    ; 4. Decrement len
+    mov eax, [len]
+    dec eax
+    mov [len], eax
 
     jmp while2
-  
+
   end_while2:
 
   mov eax, 4
@@ -109,3 +118,8 @@ _start:
   mov eax, 1
   mov ebx, 0
   int 0x80
+    
+
+  ; while len > 0
+    
+    
