08e4d6bfe77111c968fca02b9676a8175df5e109
Brymer <meneses.brymer@gmail.com>
2023-01-02 15:47:53 +0800

    improve monte-carlo

--- Haskell/monte-carlo-pi-approx/app/Main.hs
+++ Haskell/monte-carlo-pi-approx/app/Main.hs
@@ -3,15 +3,17 @@ module Main where
 import System.Random
 import System.IO
 
-piApprox :: Int -> Int -> Int -> IO Float
-piApprox currentIter maxIter numPointsInside = 
-  if currentIter < maxIter then do 
-    x <- randomRIO (0::Float, 1::Float)
-    y <- randomRIO (0::Float, 1::Float)
-    let result = numPointsInside + fromEnum( x*y + y*y < 1)
-    piApprox (currentIter + 1) maxIter result
-  else 
-    return (4 * fromIntegral numPointsInside / fromIntegral maxIter)
+piApprox :: Int -> IO Float
+piApprox maxIter = 
+  let iterMonteCarlo currentIter numPointsInside = 
+        if currentIter < maxIter then do 
+          x <- randomRIO (0::Float, 1::Float)
+          y <- randomRIO (0::Float, 1::Float)
+          let result = numPointsInside + fromEnum( x*y + y*y < 1)
+          iterMonteCarlo (currentIter + 1) result
+        else 
+          return (4 * fromIntegral numPointsInside / fromIntegral maxIter)
+  in iterMonteCarlo 0 0
 
 main :: IO ()
 main = do
@@ -20,5 +22,5 @@ main = do
   hFlush stdout
   inputString <- getLine
   let maxIter = read inputString::Int
-  pi <- piApprox 0 maxIter 0 
+  pi <- piApprox maxIter
   putStrLn ("Pi Approximation with " ++ inputString ++ " iterations is " ++ show pi)
